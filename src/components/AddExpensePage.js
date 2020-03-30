88c4d8dea18911f638cf77f687ec71aba7a9fe31
gge201 <59999590+gge201@users.noreply.github.com>
2020-03-30 14:44:49 +0530

    Add files via upload

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ExpenseForm from './ExpenseForm'
-import {addExpense} from '../actions/expenses'
+import {startAddExpense} from '../actions/expenses'
 import {connect} from 'react-redux'
 const AddExpensePage=(props)=>(
     <div>
         <h1>Add Expense</h1>
         <ExpenseForm onSubmit={(expense)=>{
-            props.dispatch(addExpense(expense))
+            props.dispatch(startAddExpense(expense))
             props.history.push('/')
         }}/>
-    </div>
+    </div> 
 );
+
 export default connect()(AddExpensePage)
